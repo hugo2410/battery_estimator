43752421f190aa0f1bf61787b1d46927f4118bb2
hugo2410 <hugobirch@hotmail.fr>
2022-11-10 08:47:43 +0100

    updates main

--- Modules/NaiveBatteryEstimation.h
+++ Modules/NaiveBatteryEstimation.h
@@ -14,6 +14,7 @@
  */
 class NaiveBatteryEstimation : public AbstractBatteryEstimation
 {
+public:
     /*
 	 * \brief: Default Constructor
 	 */
--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@
 #include <boost/functional/hash.hpp>
 #include <unordered_map>
 
-#include "main.h"
+#include "AbstractBatteryEstimation.h"
+#include "NaiveBatteryEstimation.h"
 
 
 using namespace std;
@@ -19,10 +20,19 @@ using namespace std;
 int main(int argc, char *argv[]) {
 
 
-    unordered_map<Coordinates, WindInfo, boost::hash<Coordinates>> WindData;
-    vector<Coordinates> WayPoints;
+    const unordered_map<Coordinates, WindInfo, boost::hash<Coordinates>> windData({
+                                                                                          {{12.1, 1.1}, {5.1, 2}},
+                                                                                          {{4.1, 15.1}, {7.1, 3.1}},
+                                                                                          {{5.1, -1.1}, {-5.1, -1.2}},
+                                                                                          {{11.1, 11.1}, {-15.1, 2}},
+                                                                                  });
+    const vector<Coordinates> waypoints = {{3.1, 7.1}, {2.6, 1.1}, {-1.1, 21}, {12.1, 1.1}};
 
-    computeRemainingBattery(WayPoints, WindData);
+    AbstractBatteryEstimation *pBatteryEstimation{nullptr};
+
+    pBatteryEstimation = new NaiveBatteryEstimation;
+    pBatteryEstimation->computeRemainingBattery(initialBatteryLevel, waypoints, windData, energyConsumption);
+
 
 
     return 0;
