44195cab9e864b9bd31d4b0fb2cdadf8be801059
hugo2410 <hugobirch@hotmail.fr>
2022-11-10 08:40:03 +0100

    [WIP]:starts implementation of the advanced logic

--- Modules/AdvancedBatteryEstimation.cpp
+++ Modules/AdvancedBatteryEstimation.cpp
@@ -3,3 +3,50 @@
 //
 
 #include "AdvancedBatteryEstimation.h"
+
+
+AdvancedBatteryEstimation::AdvancedBatteryEstimation(){};
+
+std::vector<double>  AdvancedBatteryEstimation::computeRemainingBattery(double initBattery,
+                                                                      const std::vector<Coordinates> &waypoints,
+                                                                      const  std::unordered_map<Coordinates,
+                                                                              WindInfo, boost::hash<Coordinates>> &windData,
+                                                                      double energyConsumption){
+
+    std::vector<double> batteryEstimation;
+    Coordinates aircraftPosition = {0,0};
+    double distance = 0, speed = 0;
+    for (auto waypoint:waypoints){
+        // Compute the distance flown between each waypoint
+        distance = computeDistance(waypoint, aircraftPosition);
+        // Update drone's position
+        aircraftPosition = waypoint;
+        // find closest wind measurement to the waypoint
+        double closestDistance = 0;
+        WindInfo closestMeasurementInfo = {0, 0};
+        for (auto windValue: windData){
+            double tmpDist = computeDistance(aircraftPosition, windValue.first);
+            if (closestDistance > tmpDist){
+                closestMeasurementInfo = windValue.second;
+                closestDistance = tmpDist;
+            }
+        }
+        speed = airSpeed + computeFacingWind(closestMeasurementInfo);
+        // Update the amount of battery left after having flown to the next waypoint
+        if (speed > 0){
+            initBattery -= (distance / speed) * (energyConsumption / SECONDSPERHOUR);
+        }
+        batteryEstimation.push_back(initBattery);
+    }
+    return batteryEstimation;
+}
+
+double  AdvancedBatteryEstimation::computeFacingWind(WindInfo windValue){
+
+    return windValue.speed * cos(windValue.direction);
+}
+
+double  AdvancedBatteryEstimation::computeDistance(Coordinates point1, Coordinates point2){
+
+    return sqrt(pow((point1.first - point2.first), 2) + pow((point1.second - point2.second), 2));
+}
--- Modules/AdvancedBatteryEstimation.h
+++ Modules/AdvancedBatteryEstimation.h
@@ -5,9 +5,45 @@
 #ifndef BATTERY_ESTIMATOR_ADVANCEDBATTERYESTIMATION_H
 #define BATTERY_ESTIMATOR_ADVANCEDBATTERYESTIMATION_H
 
+#include "AbstractBatteryEstimation.h"
 
-class AdvancedBatteryEstimation {
+class AdvancedBatteryEstimation : AbstractBatteryEstimation
+{
+public:
+    /*
+	 * \brief: Default Constructor
+	 */
+    AdvancedBatteryEstimation();
 
+    /**
+    * \brief  Simple function to compute the amount of battery left, it finds the closest point and computes the delta
+    * in ground speed due to wind
+    * @param initBattery: value of the battery at the beginning of the flight (Wh)
+    * @param WayPoints: vector containing the list of coordinates of each waypoint
+    * @param WindData: unordered map containing the coordinates of the wind measurements and the speed and direction
+    * of the measurements
+    * @param energyConsumption: value of the power needed to fly at 30m/s
+    * @return Vector containing the battery estimates at each waypoint.
+    */
+    std::vector<double> computeRemainingBattery(double initBattery,
+                                                const std::vector<Coordinates> &WayPoints,
+                                                const  std::unordered_map<Coordinates, WindInfo, boost::hash<Coordinates>> &WindData,
+                                                double energyConsumption);
+    /**
+    * \brief Computes the longitudinal component of the wind
+    * @param windValue: WindInfo object containing the speed (m/s) and the direction (rad)
+    * @return Wind speed (m/s) along the aircraft's direction
+    */
+    double computeFacingWind(WindInfo windValue);
+
+    /**
+    * \brief Computes the Euclidean distance between two points
+    * @param point1: pair of coordinate
+    * @param point2: pair of coordinate
+    * @return Distance (m) between the two points
+    */
+    double computeDistance(Coordinates point1, Coordinates point2);
+};
 };
 
 
