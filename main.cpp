88da1431ed1d479e320165d4318bebf25eeaccd8
hugo2410 <hugobirch@hotmail.fr>
2022-11-10 08:52:20 +0100

    displays results

--- main.cpp
+++ main.cpp
@@ -21,19 +21,23 @@ int main(int argc, char *argv[]) {
 
 
     const unordered_map<Coordinates, WindInfo, boost::hash<Coordinates>> windData({
-                                                                                          {{12.1, 1.1}, {5.1, 2}},
-                                                                                          {{4.1, 15.1}, {7.1, 3.1}},
-                                                                                          {{5.1, -1.1}, {-5.1, -1.2}},
-                                                                                          {{11.1, 11.1}, {-15.1, 2}},
-                                                                                  });
+                                                              {{12.1, 1.1}, {5.1, 2}},
+                                                              {{4.1, 15.1}, {7.1, 3.1}},
+                                                              {{5.1, -1.1}, {-5.1, -1.2}},
+                                                              {{11.1, 11.1}, {-15.1, 2}}, });
     const vector<Coordinates> waypoints = {{3.1, 7.1}, {2.6, 1.1}, {-1.1, 21}, {12.1, 1.1}};
 
     AbstractBatteryEstimation *pBatteryEstimation{nullptr};
 
     pBatteryEstimation = new NaiveBatteryEstimation;
-    pBatteryEstimation->computeRemainingBattery(initialBatteryLevel, waypoints, windData, energyConsumption);
-
-
-
+    std::vector<double> batteryEstimation;
+    batteryEstimation = pBatteryEstimation->computeRemainingBattery(initialBatteryLevel,
+                                                                    waypoints,
+                                                                    windData,
+                                                                    energyConsumption);
+    cout<< "Battery estimatation at each waypoint is :";
+    for (auto estimate: batteryEstimation){
+        cout<< estimate<< " ";
+    }
     return 0;
 }
